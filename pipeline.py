ca6e6adbdf003f59cbee0d16036328100e4ef223
Marjan Mohammadi <128620519+marjanUofT@users.noreply.github.com>
2026-04-21 02:59:21 -0400

    Update pipeline.py
    add visualization

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
+
 
 def fetch_data():
     url = "http://api.worldbank.org/v2/country/USA;CAN/indicator/NY.GDP.PCAP.CD?format=json"
@@ -12,6 +14,14 @@ def clean_data(df):
     df = df[['country', 'date', 'value']]
     df = df.dropna()
     return df
+
+
+def plot_data(df):
+    df = df.sort_values("date")
+    plt.plot(df["date"], df["value"])
+    plt.title("GDP per capita trend")
+    plt.xticks(rotation=45)
+    plt.show()
 
 def main():
     df = fetch_data()
